dcba487260b751fe98d5654baf3531fc10f916bb
Folarin Folarin <thefolafolarin@gmail.com>
2024-09-13 07:34:16 +0100

    personlist

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Greet } from './Components/Greet'
 import { Person } from './Components/Person'
+import {PersonList} from '/Components/PersonList'
 
 function App() {
 
@@ -9,11 +10,29 @@ function App() {
     last: 'Wayne',
   }
 
+  const nameList= {
+    {
+      first: 'Bruce'
+      last: 'Wayne'
+    }
+
+    {
+      first: 'Clark'
+      last: 'Kent'
+    }
+
+    {
+      first: 'Princess'
+      last: 'Diana'
+    }
+  }
+
 
   return (
     <>
     <Greet name ='Folarin' messageCount={20} isLoggedIn={false} />
     <Person name ={personName} />
+    <PersonList />
     </>
   )
 }
---
+++ src/Components/PersonList.jsx
@@ -0,0 +1,15 @@
+
+
+
+
+export const PersonList = () =>{
+    return(
+        <div>
+            <div>Bruce Wayne</div>
+            <div>Clark Kent</div>
+            <div>Princess Diana</div>
+        </div>
+       
+
+    )
+}
