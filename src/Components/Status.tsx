2daf98dec0bd5a8f85a9a4ab741e576e56def60e
Folarin Folarin <thefolafolarin@gmail.com>
2024-09-14 09:38:55 +0100

    statustsx

---
+++ src/Components/Status.tsx
@@ -0,0 +1,10 @@
+export const Status = () =>{
+    return(
+        <>
+        <h2>Loading...</h2>
+        <h2>Data Fetched Successfully!</h2>
+        <h2>Error fetching data</h2>
+
+        </>
+    )
+}
