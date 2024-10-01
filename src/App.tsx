ab203737e9db07e3e756480b45fedcb73b3ded2a
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-01 02:32:30 +0100

    wrk-dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,57 @@
-import './App.css'
-import { Greet } from './Components/Greet'
-import { Person } from './Components/Person'
-import { PersonList } from './Components/PersonList'
-import { Status } from './Components/Status'
-import { Heading } from './Components/Heading'
-import { Oscar } from './Components/Oscar'
-import Button from './Components/Button'
+// import './App.css'
+// import { Greet } from './Components/Greet'
+// import { Person } from './Components/Person'
+// import { PersonList } from './Components/PersonList'
+// import { Status } from './Components/Status'
+// import { Heading } from './Components/Heading'
+// import { Oscar } from './Components/Oscar'
+// import Button from './Components/Button'
 
-function App() {
+// function App() {
 
-  const personName= {
-    first: 'Bruce',
-    last: 'Wayne',
-  }
+//   const personName= {
+//     first: 'Bruce',
+//     last: 'Wayne',
+//   }
 
   
   
-    const nameList = [ 
-      {
-        first: 'Bruce',
-        last: 'Wayne',
-      },
-      {
-        first: 'Clark',
-        last: 'Kent',
-      },
-      {
-        first: 'Princess',
-        last: 'Diana',
-      },
-    ]
+//     const nameList = [ 
+//       {
+//         first: 'Bruce',
+//         last: 'Wayne',
+//       },
+//       {
+//         first: 'Clark',
+//         last: 'Kent',
+//       },
+//       {
+//         first: 'Princess',
+//         last: 'Diana',
+//       },
+//     ]
   
 
-  return (
-    <>
-    <Button handleClick={()=>{
-      console.log('Button Click')
-    }} />
-    <Greet name ='Folarin' messageCount={20} isLoggedIn={false} />
-    <Person name ={personName} />
-    <PersonList name={nameList}/>
-    <Status status='success' />
-    <Heading>
-        Placeholder text
-    </Heading>
+//   return (
+//     <>
+//     <Button handleClick={()=>{
+//       console.log('Button Click')
+//     }} />
+//     <Greet name ='Folarin' messageCount={20} isLoggedIn={false} />
+//     <Person name ={personName} />
+//     <PersonList name={nameList}/>
+//     <Status status='success' />
+//     <Heading>
+//         Placeholder text
+//     </Heading>
 
-    <Oscar>
-      <Heading>
-        Oscar goes to Leonardo DiCaprio!
-      </Heading>
-    </Oscar>
-    </>
-  )
-}
+//     <Oscar>
+//       <Heading>
+//         Oscar goes to Leonardo DiCaprio!
+//       </Heading>
+//     </Oscar>
+//     </>
+//   )
+// }
 
-export default App
+// export default App
