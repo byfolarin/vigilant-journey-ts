774a556b7e0b650e09fac4ee431afc54f5033dc6
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-06 15:42:55 +0100

    typescript

--- src/Components/state/User.tsx
+++ src/Components/state/User.tsx
@@ -7,7 +7,7 @@ interface AuthUser {
 
 export const User = () => {
 
-    const [user, setUser] = useState<AuthUser|null>(null)
+    const [user, setUser] = useState<AuthUser>({} as AuthUser)
 
     const handleLogin = () =>{
         setUser({
@@ -16,16 +16,11 @@ export const User = () => {
         })
     }
 
-    const handleLogout =()=>{
-        setUser(null)
-    }
-
   return (
     <div>
         <button onClick={()=>{handleLogin}}>Login</button>
-        <button onClick={()=>{handleLogout}}>Logout</button>
-        <div>User name is {user?.name}</div>
-        <div>User email is{user?.email}</div>
+        <div>User name is {user.name}</div>
+        <div>User email is{user.email}</div>
 
     </div>
   )
